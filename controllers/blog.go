64767c558d05b40d935d3a06dc3d7b8dc5434acf
itsky365 <itsky365@163.com>
2016-09-12 22:26:56 +0800

    go fmt

--- controllers/blog.go
+++ controllers/blog.go
@@ -3,12 +3,10 @@ package controllers
 import (
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/logs"
-	//"fmt"
 	//"github.com/astaxie/beego/orm"
 	//"beegoWork/models"
-    "github.com/astaxie/beego/orm"
-    "beegoWork/models"
-    //"fmt"
+	"beegoWork/models"
+	"github.com/astaxie/beego/orm"
 )
 
 type BlogIndexController struct {
@@ -24,33 +22,32 @@ func (c *BlogIndexController) Get() {
 	//fmt.Println("page=>", page)
 	logs.Debug("page", page)
 
+	o := orm.NewOrm()
 
-    o := orm.NewOrm()
+	user := models.User{Name: "slene22"}
 
-    user := models.User{Name: "slene22"}
+	// insert
+	user_id, err := o.Insert(&user)
+	logs.Debug("ID: %d, ERR: %v\n", user_id, err)
 
-    // insert
-    user_id, err := o.Insert(&user)
-    logs.Debug("ID: %d, ERR: %v\n", user_id, err)
+	// update
+	//user.Name = "astaxie22"
+	//num, err := o.Update(&user)
+	//logs.Debug("NUM: %d, ERR: %v\n", num, err)
 
-    // update
-    //user.Name = "astaxie22"
-    //num, err := o.Update(&user)
-    //logs.Debug("NUM: %d, ERR: %v\n", num, err)
+	// read one
+	//u := models.User{Id: user.Id}
+	//err = o.Read(&u)
+	//logs.Debug("ERR: %v\n", err)
 
-    // read one
-    //u := models.User{Id: user.Id}
-    //err = o.Read(&u)
-    //logs.Debug("ERR: %v\n", err)
-
-    //o := orm.NewOrm()
-    //logs.Debug(o)
-    ////o.Using("default") // 默认使用 default，你可以指定为其他数据库
-    //user := models.User{Name: "我是谁2312"}
-    //id, err := o.Insert(&user)
-    //if err != nil {
-    //    logs.Debug(err)
-    //}
+	//o := orm.NewOrm()
+	//logs.Debug(o)
+	////o.Using("default") // 默认使用 default，你可以指定为其他数据库
+	//user := models.User{Name: "我是谁2312"}
+	//id, err := o.Insert(&user)
+	//if err != nil {
+	//    logs.Debug(err)
+	//}
 	//logs.Debug(err)
 
 	c.Data["Id"] = id
