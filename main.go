ce5f6ae076452769c953b6b6cf8590867cc0e33a
itsky365 <itsky365@163.com>
2016-09-18 23:07:11 +0800

    user curd

--- main.go
+++ main.go
@@ -48,7 +48,7 @@ func sessionInit()  {
     //go globalSessions.GC()
     beego.BConfig.WebConfig.Session.SessionProvider = "redis"
     // 链接地址，连接池，访问密码(没有保持为空)
-    beego.BConfig.WebConfig.Session.SessionProviderConfig = "127.0.0.1:6379,0,Zhuwenzhuo0901"
+    beego.BConfig.WebConfig.Session.SessionProviderConfig = "127.0.0.1:6379,0"
 }
 
 
