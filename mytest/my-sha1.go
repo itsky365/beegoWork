8afed2ce65db24f67d5b575ae96c5c171e677b44
zhuxuehuang <itsky365@163.com>
2016-10-18 09:49:23 +0800

    sha1

---
+++ mytest/my-sha1.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+    "fmt"
+    "crypto/sha1"
+    "crypto/md5"
+    "io"
+    "sort"
+    "strings"
+)
+
+//对字符串进行MD5哈希
+func a(data string) string {
+    t := md5.New();
+    io.WriteString(t,data);
+    return fmt.Sprintf("%x",t.Sum(nil));
+}
+
+//对字符串进行SHA1哈希
+func b(data string) string {
+    t := sha1.New();
+    io.WriteString(t,data);
+    return fmt.Sprintf("%x", t.Sum(nil));
+}
+
+func main(){
+    var data string = "abc";
+    fmt.Printf("MD5 : %s\n", a(data));
+    fmt.Printf("SHA1 : %s\n", b(data));
+
+    // 字符串数组排序
+    s := []string{
+        "zz",
+        "gg",
+        "bb",
+    }
+    sort.Strings(s)
+    fmt.Println(s)
+    fmt.Println(strings.Join(s, "=="))
+}
